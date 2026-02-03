534868d2cffceed67fb32d84fec734699559de74
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-02-03 09:57:26 -0500

    learning

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-source .
+source ./common.sh  # sourcing common.sh file for reusing code
+
+chek_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo" 
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
+
+print_total_time
