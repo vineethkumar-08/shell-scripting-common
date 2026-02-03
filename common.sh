b5fcab6020ed94f15ddd9cefd9f0e6202a899208
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-02-03 09:46:52 -0500

    learning

---
+++ common.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+STAR_TIME=%(date+$s)
+
+echo "$(date "+%Y-%d-%M %H:%M:%S") |script started executing at:$(date)" | tee -a >> $LOGS_FILE
+
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+    fi
+}
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e $(date "+%Y-%d-%M %H:%M:%S") | "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e $(date "+%Y-%d-%M %H:%M:%S") "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+print_total_time(){
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$TART_TIME))
+echo -e $(date "+%Y-%d-%M %H:%M:%S") "|script executed in: $TOTAL_TIME seconds" | tee -a $LOGS_FILE
+}
